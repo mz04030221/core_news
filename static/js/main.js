905f565d30c96ea20df9cb5a5ae53cfc45f54934
Mohamed Z. <mz04030221@gmail.com>
2025-08-18 11:47:12 +0100

    Add header and nav bar

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+const WEATHER_API_KEY = "42c3a22ceb654f3d8a8192521253007";
+const WEATHER_API_CALL_URL = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=Nador&aqi=no`;
+
+const timeDiv = document.querySelector("header .time");
+const weatherDiv = document.querySelector("header .weather");
+
+setInterval(() => {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+
+  timeDiv.textContent = `${hours}:${minutes}`;
+});
+
+fetch(WEATHER_API_CALL_URL)
+  .then(async (response) => {
+    const json = await response.json();
+    weatherDiv.textContent = `${json.current.condition.text}, ${json.current.temp_c} °C`;
+  })
+  .catch((error) => console.log(`ERROR GETTING WEATHER: ${error}`));
