28937da22b956e81d99832d0eaa9955156859ac0
Mohamed Z. <mz04030221@gmail.com>
2025-08-19 22:06:18 +0100

    Make the number of time digits consistent

--- static/js/main.js
+++ static/js/main.js
@@ -6,8 +6,8 @@ const weatherDiv = document.querySelector("header .weather");
 
 setInterval(() => {
   const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
 
   timeDiv.textContent = `${hours}:${minutes}`;
 });
