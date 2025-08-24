9f2f952e79249752772ec66f47629581c877a9a7
Mohamed Z. <mz04030221@gmail.com>
2025-08-24 01:49:31 +0100

    List latest issues and articles in the home page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,32 @@
 from django.views.generic import TemplateView
+from issues.models import Issue
+from articles.models import Article
 
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomePageView, self).get_context_data(*args, **kwargs)
+        context["latest_daily_issue"] = self.get_latest_issue("DAILY")
+        context["latest_weekly_issue"] = self.get_latest_issue("WEEKLY")
+        context["latest_monthly_issue"] = self.get_latest_issue("MONTHLY")
+        return context
+
+    def get_latest_issue(self, edition):
+        issues = Issue.objects.filter(edition=edition).order_by("pk").reverse()
+        if issues.exists():
+            latest_issue = issues[0]
+        else:
+            return {}
+
+        articles = Article.objects.filter(issue=latest_issue).order_by("pk").reverse()
+        latest_articles = articles[:2] if len(articles) >= 3 else articles
+
+        return {
+            "issue": latest_issue,
+            "articles": latest_articles,
+        }
 
 
 class AboutPageView(TemplateView):
